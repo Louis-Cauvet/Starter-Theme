2b82f56b0d0d536aa9fb3d30b36675e5c1cbd35d
Louis-Cauvet <louiscauvet8@gmail.com>
2024-10-12 19:07:01 +0200

    Fix security errors in 'braces' dependency + add more accessibility in navmenu

--- marmite-src/assets/js/script-front.js
+++ marmite-src/assets/js/script-front.js
@@ -30,6 +30,14 @@ function getViewport() {
     };
 }
 
+function addNegativeTabindex($element) {
+    $element.attr('tabindex', '-1');
+}
+
+function removeTabindex($element) {
+    $element.removeAttr('tabindex');
+}
+
 // Resetting the nav-indicator's display
 function resetNavIndicator() {
     if ($('.nav-item.active').length > 0) {
@@ -53,6 +61,22 @@ function resetNavIndicator() {
     }
 }
 
+function changeNavIndicatorTarget($target) {
+    const $itemPosition = $target.position();
+    const $itemWidth = $target.outerWidth();
+    const $itemHeight = $target.outerHeight();
+
+    $('.nav-indicator').css({
+        left: $itemPosition.left + 'px',
+        top: $itemPosition.top + 'px',
+        width: $itemWidth + 'px',
+        height: $itemHeight + 'px',
+    });
+
+    $('.nav-item-name').removeClass('is-indicate');
+    $target.addClass('is-indicate');
+}
+
 // Opening/closing submenus in nav menu + moving navigation's indicator
 function manageMenuNavigationSystem() {
     if ($currentScreenWidth > 896) {
@@ -60,25 +84,13 @@ function manageMenuNavigationSystem() {
         $('.nav-item-name').on('mouseenter focus', function () {
             $('.nav-submenu').removeClass('is-open').attr('aria-expanded', 'false');
             $(this).siblings('.nav-submenu').addClass('is-open').attr('aria-expanded', 'true');
+            changeNavIndicatorTarget($(this));
         });
-        $('.nav-item-name').on('mouseenter', function () {
-            const $itemPosition = $(this).position();
-            const $itemWidth = $(this).outerWidth();
-            const $itemHeight = $(this).outerHeight();
-
-            $('.nav-indicator').css({
-                left: $itemPosition.left + 'px',
-                top: $itemPosition.top + 'px',
-                width: $itemWidth + 'px',
-                height: $itemHeight + 'px',
-            });
-
-            $('.nav-item-name').removeClass('is-indicate');
-            $(this).addClass('is-indicate');
-        })
         $('.nav-submenu a').on('focus', function () {
             $('.nav-submenu').removeClass('is-open').attr('aria-expanded', 'false');
             $(this).closest('.nav-submenu').addClass('is-open').attr('aria-expanded', 'true');
+            const $navItemParent = $(this).closest('.nav-submenu').siblings('.nav-item-name');
+            changeNavIndicatorTarget($navItemParent);
         });
         $('.inner-header').on('mouseleave', function () {
             $('.nav-submenu').removeClass('is-open').attr('aria-expanded', 'false');
@@ -90,14 +102,18 @@ function manageMenuNavigationSystem() {
         });
     } else {
         // Mobile & vertical tablet formats
+        addNegativeTabindex($('.nav-menu a, .nav-menu button'));
+
         $('.nav-item .nav-item-name').on('click', function(e) {
             if ($(this).siblings('.nav-submenu').length > 0) {
                 e.preventDefault();
                 $(this).siblings('.nav-submenu').addClass('is-open').attr('aria-expanded', 'true');
+                removeTabindex($(this).siblings('.nav-submenu').find('a, button'));
             }
         });
         $('.close-submenu').on('click', function() {
             $(this).parent('.nav-submenu').removeClass('is-open').attr('aria-expanded', 'false');
+            addNegativeTabindex($(this).parent('.nav-submenu').find('a, button'));
         });
     }
 }
@@ -167,11 +183,13 @@ const app = {
         // Open/close mobile menu
         $('.open-mobile-menu').on('click', function () {
             $('.nav-menu').addClass('is-open').attr('aria-expanded', 'true');
+            removeTabindex($('.nav-menu>ul>li>a, .nav-menu>ul>li>button'));
             $(this).addClass('as--hidden');
             $('.close-mobile-menu').removeClass('as--hidden');
         });
         $('.close-mobile-menu').on('click', function () {
             $('.nav-menu').removeClass('is-open').attr('aria-expanded', 'false');
+            addNegativeTabindex($('.nav-menu>ul>li>a, .nav-menu>ul>li>button'));
             $(this).addClass('as--hidden');
             $('.open-mobile-menu').removeClass('as--hidden');
         });
@@ -179,11 +197,13 @@ const app = {
         // Open/close searchbar
         $('.open-searchbar').on('click', function () {
             $('.searchbar').addClass('is-open').attr('aria-expanded', 'true');
+            removeTabindex($('.searchbar *'));
             $(this).addClass('as--hidden');
             $('.close-searchbar').removeClass('as--hidden');
         });
         $('.close-searchbar').on('click', function () {
             $('.searchbar').removeClass('is-open').attr('aria-expanded', 'false');
+            addNegativeTabindex( $('.searchbar input, .searchbar button'));
             $(this).addClass('as--hidden');
             $('.open-searchbar').removeClass('as--hidden');
         });
